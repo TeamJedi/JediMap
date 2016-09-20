69b23949f1fa25e5bef2e45ed0a2bf640bb5bfca
Ian Blenke <ian@blenke.com>
2016-09-19 20:55:01 -0400

    Removing osx platform, removing cordova-plugin-device and cordova-plugin-background-mode

--- src/platforms/android/platform_www/cordova_plugins.js
+++ src/platforms/android/platform_www/cordova_plugins.js
@@ -13,23 +13,6 @@ module.exports = [
         "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
         "pluginId": "cordova-plugin-geolocation",
         "runs": true
-    },
-    {
-        "id": "cordova-plugin-device.device",
-        "file": "plugins/cordova-plugin-device/www/device.js",
-        "pluginId": "cordova-plugin-device",
-        "clobbers": [
-            "device"
-        ]
-    },
-    {
-        "id": "cordova-plugin-background-mode.BackgroundMode",
-        "file": "plugins/cordova-plugin-background-mode/www/background-mode.js",
-        "pluginId": "cordova-plugin-background-mode",
-        "clobbers": [
-            "cordova.plugins.backgroundMode",
-            "plugin.backgroundMode"
-        ]
     }
 ];
 module.exports.metadata = 
@@ -38,9 +21,7 @@ module.exports.metadata =
     "cordova-plugin-whitelist": "1.2.2",
     "cordova-plugin-crosswalk-webview": "2.0.0",
     "cordova-plugin-compat": "1.0.0",
-    "cordova-plugin-geolocation": "2.3.0",
-    "cordova-plugin-device": "1.1.3",
-    "cordova-plugin-background-mode": "0.6.5"
+    "cordova-plugin-geolocation": "2.3.0"
 };
 // BOTTOM OF METADATA
 });
