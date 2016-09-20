2fc4a9041fdc024100a030b49846fca2663a76a4
Ian Blenke <ian@blenke.com>
2016-09-19 22:45:52 -0400

    cordova plugin add cordova-plugin-background-mode

--- src/platforms/browser/platform_www/cordova_plugins.js
+++ src/platforms/browser/platform_www/cordova_plugins.js
@@ -1,12 +1,38 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/src/browser/DeviceProxy.js",
+        "id": "cordova-plugin-device.DeviceProxy",
+        "pluginId": "cordova-plugin-device",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-background-mode/www/background-mode.js",
+        "id": "cordova-plugin-background-mode.BackgroundMode",
+        "pluginId": "cordova-plugin-background-mode",
+        "clobbers": [
+            "cordova.plugins.backgroundMode",
+            "plugin.backgroundMode"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.2.2",
     "cordova-plugin-crosswalk-webview": "2.0.0",
     "cordova-plugin-compat": "1.0.0",
-    "cordova-plugin-geolocation": "2.3.0"
+    "cordova-plugin-geolocation": "2.3.0",
+    "cordova-plugin-device": "1.1.3",
+    "cordova-plugin-background-mode": "0.6.5"
 }
 // BOTTOM OF METADATA
 });
